7cb4222979a808c77e53125ab0f2888ef7447eb1
A.Unger <zyxancf@gmail.com>
2019-12-04 16:00:37 +0100

    POC working

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/refs/cs3-micro
 
 go 1.13
 
+replace github.com/fatih/color v0.0.0-20190410021324-65acae22fc9 => github.com/gogo/protobuf v0.0.0-20190723190241-65acae22fc9d
+
 require (
+	github.com/cs3org/reva v0.0.1
 	github.com/golang/protobuf v1.3.2
 	github.com/micro/go-micro v1.17.1
 	google.golang.org/grpc v1.25.1
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	demoAuth "github.com/cs3org/reva/pkg/auth/manager/demo"
+	userManager "github.com/cs3org/reva/pkg/user/manager/demo"
+	"github.com/micro/go-micro"
+	providerv1beta1 "github.com/refs/cs3-micro/pkg/cs3/auth/provider/v1beta1"
+	userv1beta1 "github.com/refs/cs3-micro/pkg/cs3/identity/user/v1beta1"
+)
+
+type authHandler struct{}
+
+func (h *authHandler) Authenticate(ctx context.Context, req *providerv1beta1.AuthenticateRequest, res *providerv1beta1.AuthenticateResponse) error {
+	demoManager, _ := demoAuth.New(nil)
+	// forward request to Demo manager
+	// userid, err := demoManager.Authenticate(context.TODO(), req.GetClientId(), req.GetClientSecret())
+	userid, err := demoManager.Authenticate(context.TODO(), "einstein", "relativity")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// we have a user id, how do we get the user from the user id?
+	um, err := userManager.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := um.GetUser(context.TODO(), userid)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res.User = &userv1beta1.User{
+		Username: user.GetDisplayName(),
+	}
+
+	return nil
+}
+
+func main() {
+	// behind a gateway
+	service := micro.NewService(
+		micro.Name("go.micro.api.authprovider"),
+		micro.Version("latest"),
+	)
+
+	service.Init()
+
+	// handler registration happens only after service initialization...obviously
+	err := providerv1beta1.RegisterProviderAPIHandler(service.Server(), new(authHandler))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
